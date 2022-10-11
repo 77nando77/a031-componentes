dd262a34485b045e174915d604f891d9662427ac
luizfk <luiz.kessler7@gmail.com>
2022-10-11 12:53:26 -0300

    react2

---
+++ src/InfosUsuario.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function InfoUsuario(){
+    const titulo = "Título do vídeo";
+
+    return(
+        <>
+            <img src="https://picsum.photos/400/400?a=1 " alt="" />
+            <h4>{titulo}</h4>
+        </>
+    )
+}
